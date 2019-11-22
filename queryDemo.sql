84d604156d8d56434f9a684860a5debd52814068
Pumbaazz <nguyntunphng@gmail.com>
2019-11-23 00:44:22 +0700

    commit new
    nothing special here

--- queryDemo.sql
+++ queryDemo.sql
@@ -2,7 +2,35 @@
 SELECT SUM(snn.totaldue) AS "Online Order" FROM dbo.n_salesorderheader AS snn WHERE snn.onlineorderflag = 1
 SELECT SUM(snn.totaldue) AS "Offline Order" FROM dbo.n_salesorderheader AS snn WHERE snn.onlineorderflag = 0
 --cau d ne
-SELECT snn.customerid FROM n_salesorderheader snn JOIN n_salesorderdetail AS snl ON(snn.salesorderid = snl.salesorderid) WHERE snl.productid = (SELECT cdl.productid from dbo.n_product AS cdl WHERE cdl.name LIKE 'Water Bottle - 30 oz.') GROUP BY snn.customerid
+SELECT snn.customerid 
+FROM n_salesorderheader snn JOIN n_salesorderdetail AS snl ON(snn.salesorderid = snl.salesorderid) 
+WHERE snl.productid = 
+	(SELECT cdl.productid 
+	FROM dbo.n_product AS cdl 
+	WHERE cdl.name LIKE 'Water Bottle - 30 oz.') 
+GROUP BY snn.customerid
 
 SELECT * FROM n_salesorderdetail ORDER BY modifieddate DESC
 SELECT * FROM n_salesorderheader ORDER BY modifieddate DESC 
+
+--cau d ne
+SELECT snn.customerid 
+FROM n_salesorderheader snn JOIN n_salesorderdetail AS snl ON(snn.salesorderid = snl.salesorderid) 
+WHERE (snl.productid = 
+	(SELECT cdl.productid 
+	FROM dbo.n_product AS cdl 
+	WHERE cdl.name LIKE 'Water Bottle - 30 oz.') )
+GROUP BY snn.customerid
+
+-- cau e ne
+SELECT  snn.modifieddate ,snn.customerid
+FROM n_salesorderheader snn JOIN n_salesorderdetail snl ON(snn.salesorderid = snl.salesorderid) 
+WHERE (snl.productid = 
+	(SELECT cdl.productid 
+	FROM dbo.n_product AS cdl 
+	WHERE cdl.name LIKE 'Water Bottle - 30 oz.') and (snn.modifieddate > '20130501' and snn.modifieddate > '20130801') )
+GROUP BY snn.customerid, snn.ModifiedDate
+
+SELECT ModifiedDate FROM dbo.n_SalesOrderHeader ORDER BY ModifiedDate ASC
+
+CREATE PARTITION FUNCTION partitionIndex(DATETIME) AS RANGE RIGHT FOR VALUES ('20110101', '20120202', '20130101', '20140101', '20150101')
