99e58ab9a3f52440d62c97c22fb0dc1bb830cf0b
Tuan Phung <37480725+Pumbaazz@users.noreply.github.com>
2019-11-22 14:51:27 +0700

    update
    add the g-2 nonclustered index

---
+++ Index_SaleManagement_db_sv.sql
@@ -0,0 +1,145 @@
+create database "2019_CSDLNC_DA1_GROUP5_Index"
+
+ON (NAME = '2019_CSDLNC_DA1_GROUP5', FILENAME = 'D:\okela\2019_CSDLNC_DA1_GROUP5_Index.mdf')
+LOG ON (NAME = '2019_CSDLNC_DA1_GROUP5_log', FILENAME = 'D:\okela\2019_CSDLNC_DA1_GROUP5_Index_log.ldf')
+GO
+--drop database iSaleManagement
+use "2019_CSDLNC_DA1_GROUP5_Index"
+go
+--drop table [n_SalesOrderHeader]
+
+CREATE TYPE [dbo].[Flag] FROM [bit] NOT NULL
+GO
+CREATE TYPE [dbo].[OrderNumber] FROM [nvarchar](25) NULL
+GO
+CREATE TYPE [dbo].[AccountNumber] FROM [nvarchar](15) NULL
+GO
+CREATE TYPE [dbo].[Name] FROM [nvarchar](50) NULL
+GO
+CREATE TABLE [n_Product](
+	[ProductID] [int] NOT NULL,
+	[Name] [dbo].[Name] NOT NULL,
+	[ProductNumber] [nvarchar](25) NOT NULL,
+	[MakeFlag] [dbo].[Flag] NOT NULL,
+	[FinishedGoodsFlag] [dbo].[Flag] NOT NULL,
+	[Color] [nvarchar](15) NULL,
+	[SafetyStockLevel] [smallint] NOT NULL,
+	[ReorderPoint] [smallint] NOT NULL,
+	[StandardCost] [money] NOT NULL,
+	[ListPrice] [money] NOT NULL,
+	[Size] [nvarchar](5) NULL,
+	[SizeUnitMeasureCode] [nchar](3) NULL,
+	[WeightUnitMeasureCode] [nchar](3) NULL,
+	[Weight] [decimal](8, 2) NULL,
+	[DaysToManufacture] [int] NOT NULL,
+	[ProductLine] [nchar](2) NULL,
+	[Class] [nchar](2) NULL,
+	[Style] [nchar](2) NULL,
+	[ProductSubcategoryID] [int] NULL,
+	[ProductModelID] [int] NULL,
+	[SellStartDate] [datetime] NOT NULL,
+	[SellEndDate] [datetime] NULL,
+	[DiscontinuedDate] [datetime] NULL,
+	[rowguid] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_Product_ProductID] PRIMARY KEY CLUSTERED 
+(
+	[ProductID] ASC
+)
+)
+go
+--drop table [n_SalesOrderHeader]
+CREATE TABLE [n_SalesOrderHeader](
+	[SalesOrderID] [int] NOT NULL,
+	[RevisionNumber] [tinyint] NOT NULL,
+	[OrderDate] [datetime] NOT NULL,
+	[DueDate] [datetime] NOT NULL,
+	[ShipDate] [datetime] NULL,
+	[Status] [tinyint] NOT NULL,
+	[OnlineOrderFlag] [dbo].[Flag] NOT NULL,
+	[SalesOrderNumber]  varchar(20),--AS (isnull(N'SO'+CONVERT([nvarchar](23),[SalesOrderID]),N'*** ERROR ***')),
+	[PurchaseOrderNumber] [dbo].[OrderNumber] NULL,
+	[AccountNumber] [dbo].[AccountNumber] NULL,
+	[CustomerID] [int] NOT NULL,
+	[SalesPersonID] [int] NULL,
+	[TerritoryID] [int] NULL,
+	[BillToAddressID] [int] NOT NULL,
+	[ShipToAddressID] [int] NOT NULL,
+	[ShipMethodID] [int] NOT NULL,
+	[CreditCardID] [int] NULL,
+	[CreditCardApprovalCode] [varchar](15) NULL,
+	[CurrencyRateID] [int] NULL,
+	[SubTotal] [money] NOT NULL,
+	[TaxAmt] [money] NOT NULL,
+	[Freight] [money] NOT NULL,
+	[TotalDue] [money], --AS (isnull(([SubTotal]+[TaxAmt])+[Freight],(0))),
+	[Comment] [nvarchar](128) NULL,
+	[rowguid] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_SalesOrderHeader_SalesOrderID] PRIMARY KEY CLUSTERED 
+(
+	[SalesOrderID] ASC
+)
+) 
+go
+--drop table [n_SalesOrderDetail]
+CREATE TABLE [n_SalesOrderDetail](
+	[SalesOrderID] [int] NOT NULL,
+	[SalesOrderDetailID] [int] NOT NULL,
+	[CarrierTrackingNumber] [nvarchar](25) NULL,
+	[OrderQty] [smallint] NOT NULL,
+	[ProductID] [int] NOT NULL,
+	[SpecialOfferID] [int] NOT NULL,
+	[UnitPrice] [money] NOT NULL,
+	[UnitPriceDiscount] [money] NOT NULL,
+	[LineTotal] decimal, --AS (isnull(([UnitPrice]*((1.0)-[UnitPriceDiscount]))*[OrderQty],(0.0))),
+	[rowguid] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_SalesOrderDetail_SalesOrderID_SalesOrderDetailID] PRIMARY KEY CLUSTERED 
+(
+	[SalesOrderID] ASC,
+	[SalesOrderDetailID] ASC
+)
+) 
+go
+CREATE FUNCTION [dbo].[ufnLeadingZeros](
+    @Value int
+) 
+RETURNS varchar(8) 
+WITH SCHEMABINDING 
+AS 
+BEGIN
+    DECLARE @ReturnValue varchar(8);
+
+    SET @ReturnValue = CONVERT(varchar(8), @Value);
+    SET @ReturnValue = REPLICATE('0', 8 - DATALENGTH(@ReturnValue)) + @ReturnValue;
+
+    RETURN (@ReturnValue);
+END;
+go
+--drop table [n_Customer]
+CREATE TABLE [n_Customer](
+	[CustomerID] [int] NOT NULL,
+	[PersonID] [int] NULL,
+	[StoreID] [int] NULL,
+	[TerritoryID] [int] NULL,
+	[AccountNumber]  varchar(20),--AS (isnull('AW'+[dbo].[ufnLeadingZeros]([CustomerID]),'')),
+	[rowguid] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_Customer_CustomerID] PRIMARY KEY CLUSTERED 
+(
+	[CustomerID] ASC
+)
+)
+go
+
+CREATE NONCLUSTERED INDEX pRoDuCtNaMe_INDEX ON n_product(Name)
+GO
+
+SELECT snn.customerid 
+FROM n_salesorderheader snn JOIN n_salesorderdetail AS snl ON(snn.salesorderid = snl.salesorderid) 
+WHERE snl.productid = 
+	(SELECT cdl.productid 
+	FROM dbo.n_product AS cdl 
+	WHERE cdl.name LIKE 'Water Bottle - 30 oz.') 
+GROUP BY snn.customerid
