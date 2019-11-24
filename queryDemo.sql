e757642c8cf8ae5a191c17123ddcfae074511253
Pumbaazz <nguyntunphng@gmail.com>
2019-11-24 15:36:03 +0700

    update i question

--- queryDemo.sql
+++ queryDemo.sql
@@ -1,25 +1,3 @@
-
--- cau a ne
-Select sod.ProductID, SUM(sod.UnitPrice) as TongTien
-From dbo.n_SalesOrderDetail as sod join dbo.n_Product as pro
-on(sod.ProductID = pro.ProductID) join dbo.n_SalesOrderHeader as soh
-on(sod.SalesOrderID = soh.SalesOrderID)
-where soh.DueDate < '2014-05-15'
-Group by sod.ProductID 
-Order by sod.ProductID ASC
-
-
-Select sod.ProductID, SUM(sod.UnitPriceDiscount) as TongTien
-From dbo.n_SalesOrderDetail as sod join dbo.n_Product as pro
-on(sod.ProductID = pro.ProductID) join dbo.n_SalesOrderHeader as soh
-on(sod.SalesOrderID = soh.SalesOrderID)
-where soh.DueDate < '2014-05-15'
-Group by sod.ProductID 
-Order by sod.ProductID ASC
-
--- cau b ne
-
-
 --cau c ne
 SELECT SUM(snn.totaldue) AS "Online Order" FROM dbo.n_salesorderheader AS snn WHERE snn.onlineorderflag = 1
 SELECT SUM(snn.totaldue) AS "Offline Order" FROM dbo.n_salesorderheader AS snn WHERE snn.onlineorderflag = 0
@@ -45,5 +23,14 @@ WHERE (snl.productid =
 GROUP BY snn.customerid, snn.ModifiedDate
 
 SELECT ModifiedDate FROM dbo.n_SalesOrderHeader ORDER BY ModifiedDate ASC
-
-CREATE PARTITION FUNCTION partitionIndex(DATETIME) AS RANGE RIGHT FOR VALUES ('20110101', '20120202', '20130101', '20140101', '20150101')
+--cau i demo
+--partition p2 include date 2013-05-01 < date < 2013-09-01
+CREATE PARTITION FUNCTION partitionIndex(DATETIME) AS RANGE LEFT FOR VALUES ('20130501', '20130901')
+GO 
+SELECT snn.customerid, snn.modifieddate 
+FROM n_salesorderheader snn JOIN n_salesorderdetail snl ON(snn.salesorderid = snl.salesorderid) 
+WHERE (snl.productid = 
+	(SELECT cdl.productid 
+	FROM dbo.n_product AS cdl WITH PARTITION('p2')
+	WHERE cdl.name LIKE 'Water Bottle - 30 oz.') )
+GROUP BY snn.customerid, snn.ModifiedDate
